95ca045d63d46255d343fb80767a97a0524b4bff
oleksiibaida <baidarka28@gmail.com>
2024-11-30 13:33:44 +0100

    esp32 init

---
+++ code/esp/m5stick/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <M5StickC.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
+
+void setup() {
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
