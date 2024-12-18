9718ce90b6929f9c6a375d82c23784699db3ed8d
oleksiibaida <baidarka28@gmail.com>
2024-12-19 00:14:03 +0100

    start tailwind

--- code/web/app/forms.py
+++ code/web/app/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
-
 from config.logger_config import logger_init
 _logger = logger_init()
 class RegistrationForm(FlaskForm):
